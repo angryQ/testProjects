7fd24d2abe2ff55f3637842fcd8f8b18727de610
Kristina <kudinova2807@gmail.com>
2020-09-14 19:42:17 +0700

    Initial

---
+++ MenuCafe/MenuCafe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MenuCafe
+//
+//  Created by Kristina Kudinova on 14.09.2020.
+//  Copyright © 2020 Kristina Kudinova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
